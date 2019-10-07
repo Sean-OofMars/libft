5019d52bea0b27bc7ddf82484a333463a012c880
Sean-OofMars <31665559+Sean-OofMars@users.noreply.github.com>
2019-10-07 13:49:44 -0700

    Add files via upload

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: sferreir <sferreir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/19 10:43:14 by sferreir          #+#    #+#             */
-/*   Updated: 2019/10/07 10:38:16 by sferreir         ###   ########.fr       */
+/*   Updated: 2019/10/07 13:48:43 by sferreir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,27 +15,12 @@
 
 char	*ft_strrchr(const char *s, int c)
 {
-	int				i;
-	unsigned char	*a;
-	unsigned char	x;
-	unsigned char	*p;
+	int		len;
 
-	x = (unsigned char)c;
-	a = (unsigned char*)s;
-	i = 0;
-	while (a[i])
-	{
-		if (a[i] == x)
-			p = &a[i];
-		i++;
-	}
-	if (x == '\0')
-	{
-		i = strlen(s);
-		p = &a[i];
-	}
-	if (p)
-		return ((char*)p);
-	else
-		return (NULL);
+	len = ft_strlen((char *)s);
+	while (0 != len && s[len] != (char)c)
+		len--;
+	if (s[len] == (char)c)
+		return ((char *)&s[len]);
+	return (NULL);
 }
